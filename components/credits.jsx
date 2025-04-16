9c621451b27531f6548d334dcab896086e9d7f1a
Redaksi-Official <squadredaksi2023@gmail.com>
2025-04-16 08:38:18 +0700

    Perubahan section

--- components/credits.jsx
+++ components/credits.jsx
@@ -5,10 +5,10 @@ const Credits = (props) => {
         <div data-section id='credits' className='group mt-32'>
             <div className='text-surface-600'>
                 <div className='mb-4'>
-                    Website ini gue buat dengan <a  target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">sepenuh hati.</a> <br/> Created by <a href="https://www.instagram.com/d.dexx_/" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">d.dexx_</a> using <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Next.js</a> and 
-                    <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500"> Tailwind CSS</a>. 
-                    The inspiration for the design comes from <a  target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Otak </a> 
-                    dan <a target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Hati</a>. 
+                    This website was built with <a  target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">love.</a> <br/> Created by <a href="https://www.instagram.com/d.dexx_/" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">d.dexx_</a> using <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Next JS, React JS</a> and 
+                    <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500"> Tailwind. </a> 
+                    The inspiration for the design comes from <a  target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500"> Imagination </a> 
+                    dan <a target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Heart</a>. 
                 </div>
                 <div className='flex flex-row justify-between'>
                     <a href="/imprint" rel="noopener noreferrer" className="scroll-auto transition-all hover:text-on-background">Imprint</a>
